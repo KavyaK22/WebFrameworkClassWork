2838431624cc931f5cad9d37d29b69a944306208
KavyaK22 <152161513+KavyaK22@users.noreply.github.com>
2024-04-24 09:15:27 +0530

    Delete day5 directory

--- day5/DAY5/class_exercise/ex1/src/main/java/com/example/ex1/repository/ProductRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.ex1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ex1.model.Product;
-
-public interface ProductRepo extends JpaRepository<Product,Integer>{
-    
-}
